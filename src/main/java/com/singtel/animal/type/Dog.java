60336c1ac6d053dac0d99565bd94ee876cae71a0
Suria <suriajb86@gmail.com>
2018-08-11 12:36:02 +0800

    Code clean up

--- src/main/java/com/singtel/animal/type/Dog.java
+++ src/main/java/com/singtel/animal/type/Dog.java
@@ -4,7 +4,7 @@ import com.singtel.animal.group.Mammals;
 
 public class Dog extends Mammals {
 	
-	private static String sound = "Woof, woof";
+	private String sound = "Woof, woof";
 	
 	public Dog() {
 		super.attr.setSound(sound);
